bc070613111aa92dde8f8a3bae424f7f1a6a9913
wrijus <wrijus1@yahoo.com>
2018-04-13 15:57:27 -0500

    It is kinda working

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ app.config['DEBUG'] = True
 tasks = []
 
 @app.route('/', methods=['POST', 'GET'])
-def index():
+def list_of_posts():
+
+
+
+    return render_template('main_blog.html',title="Build a smegging Blog!", tasks=tasks)
+
+
+@app.route('/add_blog', methods=['POST', 'GET'])
+def add_new_post():
 
     if request.method == 'POST':
         task = request.form['task']
         tasks.append(task)
 
-    return render_template('main_blog.html',title="Build a smegging Blog!", tasks=tasks)
-
-
-@app.route('/add_blog')
-def add_new_post():
-
-
-    return render_template('add_blog.html')
+    return render_template('add_blog.html', tasks=tasks)
 
 
 app.run()
