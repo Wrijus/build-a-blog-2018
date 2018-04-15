c18ffd8dc5e6ab8a27cce77408100dfbd408c8ee
wrijus <wrijus1@yahoo.com>
2018-04-15 13:22:00 -0500

    set up db and tested with task

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2018:makeit2sec3@localhost:8889/build-a-blog2018'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 tasks = []
 
@@ -23,5 +35,5 @@ def add_new_post():
 
     return render_template('add_blog.html', tasks=tasks)
 
-
-app.run()
+if __name__ == '__main__':
+    app.run()
