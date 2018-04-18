5d8c4d97f3e50f02357a89c2ff27c139dcf4c6c3
wrijus <wrijus1@yahoo.com>
2018-04-18 17:42:09 -0500

    still working on getting single post to show

--- main.py
+++ main.py
@@ -7,40 +7,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2018:makeit
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-class Task(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(300))
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 
 
 @app.route('/blog', methods=['POST', 'GET'])
 def list_of_posts():
+  
+    blogs = Blog.query.all()
+    one_entry = Blog.query.filter_by(id=3).first()
+    return render_template('blog.html',title="Build a smegging Blog!", blogs=blogs, one_entry=one_entry)
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-    
-    tasks = Task.query.all()
+@app.route('/test/<int:id>', methods=['GET'])
+def pull_entry(id):
 
-    return render_template('blog.html',title="Build a smegging Blog!", tasks=tasks)
-
+     
+    blogs = Blog.query.filter_by(id=id).first()
+    return render_template('entry.html', blogs=blogs)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def add_new_post():
 
 
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)        
-        db.session.add(new_task)
+        title = request.form['title']
+        body = request.form['body']
+        new_blog = Blog(title, body)   
+        db.session.add(new_blog)
         db.session.commit()
 
-    tasks = Task.query.all()
-
+    blogs = Blog.query.all()
     return render_template('newpost.html')
 
 if __name__ == '__main__':
